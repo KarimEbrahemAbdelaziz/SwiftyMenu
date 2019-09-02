b57654d06713f3ff4791dfd369c973a8c39b0d96
KarimEbrahemAbdelaziz <karimabdelazizmansour@gmail.com>
2019-09-02 15:01:33 +0200

    Write Docs for SwiftyMenuDisplayable protocol

--- SwiftyMenu/Classes/SwiftMenuDisplayable.swift
+++ SwiftyMenu/Classes/SwiftMenuDisplayable.swift
@@ -7,8 +7,18 @@
 
 import Foundation
 
-// Markable interface for pathing datasource objects in late binding
-public protocol SwiftMenuDisplayable {
-    var displayValue: String { get }
-    var valueToRetrive: Any { get }
+/**
+ Markable interface allow custom types to be used with SwiftyMenu
+ 
+ ## Important Notes ##
+ 1. Conform this protocol in the type you want to use in the Menu.
+ 2. displayedValue: This is the value from your type will be displayed in the Menu.
+ 3. retrivedValue: This is the value will be returned when select Menu Item.
+*/
+public protocol SwiftyMenuDisplayable {
+    /// The value that will be displayed in the menu item
+    var displayedValue: String { get }
+    
+    /// The value that will be returned when select menu item
+    var retrivedValue: Any { get }
 }
