479f1183f600b5290fd74228ec23b52bf3311d6d
KarimEbrahemAbdelaziz <karimabdelazizmansour@gmail.com>
2020-03-24 22:32:58 +0200

    Update SwiftyMenuDisplayable file

--- SwiftyMenu/Classes/SwiftMenuDisplayable.swift
+++ SwiftyMenu/Classes/SwiftMenuDisplayable.swift
@@ -1,20 +1,30 @@
 //
 //  SwiftMenuDisplayable.swift
-//  Pods-SwiftyMenu_Example
 //
-//  Created by Amr Elghadban (AmrAngry) on 5/31/19.
+//  Copyright (c) 2019-2020 Karim Ebrahem (https://twitter.com/k_ebrahem_)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 import Foundation
 
-/**
- Markable interface allow custom types to be used with SwiftyMenu
- 
- ## Important Notes ##
- 1. Conform this protocol in the type you want to use in the Menu.
- 2. displayableValue: This is the value from your type will be displayed in the Menu.
- 3. retrivableValue: This is the value will be returned when select Menu Item.
-*/
+/// `SwiftyMenuDisplayable` is the markable interface to allow custom types to be used with SwiftyMenu.
 public protocol SwiftyMenuDisplayable {
     /// The value that will be displayed in the menu item
     var displayableValue: String { get }
